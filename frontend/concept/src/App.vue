512d096c5b54787316bf57c7e5d93d88c72593ee
MariaOussadi <mariaoussadi@gmail.com>
2023-01-08 21:44:35 +0100

    added the vue project

---
+++ frontend/concept/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-main class=" background">
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+
+<style scoped>
+.v-application{
+  font-family:  'Poppins', sans-serif;
+}
+.v-main{
+  align-items: center;
+}
+</style>
---
+++ frontend/concept/src/views/SignInView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="signin">
+    <v-container class="d-flex flex-column align-center align-lg-center">
+      <v-card class="rounded-xl card pa-5" flat>
+        <v-card-text>
+          <img src="../assets/ConceptLogo.png" width="250" />
+          <h2 class="secondary--text mt-10">Welcome back!</h2>
+          <p class="grey--text mb-10 mt-5">Login to your account</p>
+          <v-form>
+            <v-text-field
+              prepend-icon="mdi-account"
+              name="login"
+              label="Username"
+              type="text"
+              class="rounded-lg"
+              background-color="background"
+              height="45"
+              outlined
+            ></v-text-field>
+            <v-text-field
+              id="password"
+              prepend-icon="mdi-lock"
+              name="password"
+              label="Password"
+              type="password"
+              background-color="background"
+              class="rounded-lg"
+              height="45"
+              outlined
+            ></v-text-field>
+          </v-form>
+          <v-btn text block>Forgot Password?</v-btn>
+          <!--login button-->
+          <v-btn color="primary" class="mt-8 rounded-lg" width="150" height="45"
+            >Login</v-btn
+          >
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+
+<style scoped>
+.signin {
+  width: 100%;
+  height: 100%;
+}
+.card {
+  width: 500px;
+  text-align: center;
+}
+h2 {
+  font-size: 25px;
+}
+p {
+  font-size: 15px;
+}
+.v-container{
+  height: 100%;
+}
+.v-text-field{
+  border: none;
+}
+</style>
