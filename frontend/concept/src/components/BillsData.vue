b90962ca426b524d9ab9fcfcd245b8359bcb5a11
SaadiMahdi <saadimahdi2002@gmail.com>
2023-01-29 12:39:32 +0100

    i want pizza

--- frontend/concept/src/components/BillsData.vue
+++ frontend/concept/src/components/BillsData.vue
@@ -3,74 +3,127 @@
     <template v-slot:top>
       <v-toolbar flat>
         <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
-              New Bill
+        <template>
+            <v-btn color="primary" dark class="mb-2" @click="InsertItem()">
+              New bill
             </v-btn>
           </template>
-          <v-card>
-            <v-card-title>
-              <span class="">New Bill</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-dialog ref="dialog" v-model="modal" persistent width="290px">
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-text-field v-model="bill.date" format="dd/MM/yyyy" label="Insert Date"
-                        prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on" outlined></v-text-field>
-                    </template>
-                    <v-date-picker v-model="bill.date" scrollable format="dd/MM/yyyy">
-                      <v-spacer></v-spacer>
-                      <v-btn text color="primary" @click="modal = false">
-                        Cancel
-                      </v-btn>
-                      <v-btn text color="primary" @click="$refs.dialog.save(bill.date)">
-                        OK
-                      </v-btn>
-                    </v-date-picker>
-                  </v-dialog>
-                </v-row>
-                <v-row>
-                  <v-select :items="vendors" v-model="bill.fournisseur" label="fournisseur" outlined return-object
-                    item-text="name" item-value="_id"></v-select>
-                </v-row>
-                <v-row>
-                  <v-select :items="prod" v-model="bill.listeProduits" label="Choose Products" outlined multiple chips
-                    return-object item-text="designation" item-value="_id"></v-select>
-                  <v-container v-for="produit in bill.listeProduits" :key="produit._id">
-                    <v-row>
-                      <v-col>
-                        <v-container>
-                          <p>Product: {{ produit.designation }}</p>
-                        </v-container>
-                      </v-col>
-                      <v-col>
-                        <v-text-field v-model="produit.quantite" type="number" label="Quantity" outlined></v-text-field>
-                      </v-col>
-                      <v-col>
-                        <v-text-field v-model="produit.prixAchat" type="number" label="Purchase Price"
-                          outlined></v-text-field>
-                      </v-col>
-                      <v-col>
-                        <v-text-field v-model="produit.prixVente" type="number" label="Selling Price"
-                          outlined></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" text @click="close"> Cancel </v-btn>
-              <v-btn color="primary" @click="save"> Save </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+          <v-dialog v-model="dialog" max-width="1000px" height="1300px">
+            <v-card>
+              <v-card-title>
+                <span class="">Add bill</span>
+              </v-card-title>
+              <v-form action="" method="post" ref="form">
+                <v-container fluid>
+                  <v-row>
+                    <v-col cols="6">
+                      <!-- first column content -->
+                      <v-select
+                        :items="vendors"
+                        v-model="bill.fournisseur"
+                        label="Vendor"
+                        outlined
+                        return-object
+                        item-text="name"
+                        item-value="_id"
+                      ></v-select>
+                      <v-dialog
+                        ref="dialog"
+                        v-model="modal"
+                        persistent
+                        width="290px"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="bill.date"
+                            format="dd/MM/yyyy"
+                            label="Insert Date"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                            outlined
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="bill.date"
+                          scrollable
+                          format="dd/MM/yyyy"
+                        >
+                          <v-spacer></v-spacer>
+                          <v-btn text color="primary" @click="modal = false">
+                            Cancel
+                          </v-btn>
+                          <v-btn
+                            text
+                            color="primary"
+                            @click="$refs.dialog.save(bill.date)"
+                          >
+                            OK
+                          </v-btn>
+                        </v-date-picker>
+                      </v-dialog>
+                    </v-col>
+                    <v-col cols="6" class="d-flex flex-column">
+                      <h3 class="mb-3">Items</h3>
+                      <v-select
+                        :items="prod"
+                        v-model="bill.listeProduits"
+                        label="Choose Products"
+                        outlined
+                        multiple
+                        chips
+                        return-object
+                        item-text="designation"
+                        item-value="produit._id"
+                      ></v-select>
+                      <v-container
+                        v-for="produit in bill.listeProduits"
+                        :key="produit._id"
+                      >
+                        <v-row>
+                          <v-col>
+                            <v-container>
+                              <h3>{{ produit?.designation }}</h3>
+                              <p class="grey--text">
+                                {{ produit.categorie?.designation }}
+                              </p>
+                            </v-container>
+                          </v-col>
+                          <v-col>
+                            <v-text-field
+                              v-model="produit.quantite"
+                              type="number"
+                              label="Quantity"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col>
+                            <v-text-field
+                              v-model="produit.prixAchat"
+                              type="number"
+                              label="Bought price"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col>
+                            <v-text-field
+                              v-model="produit.prixVente"
+                              type="number"
+                              label="Sold price"
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="close"> Cancel </v-btn>
+                <v-btn color="primary" @click="save"> Save </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         <v-dialog v-model="dialogEdit" max-width="500px">
           <v-card>
             <v-card-title>
@@ -142,7 +195,7 @@
         <v-dialog v-model="dialogDetails" max-width="1000px">
           <v-card>
             <v-card-title>
-              <span class="headline">Bill Details</span>
+              <span class="">Bill Details</span>
             </v-card-title>
             <v-card-text>
               <v-container>
@@ -177,9 +230,10 @@
                       <v-row>
                         <v-col>
                           <v-container>
-                            <p>Product: {{ produit.produit.designation }}</p>
+                            <p>Product: {{ produit.designation }}</p>
                           </v-container>
                         </v-col>
+
                         <v-col>
                           <v-container>
                             <p>Quantity: {{ produit.quantite }}</p>
@@ -192,12 +246,16 @@
                         </v-col>
                       </v-row>
                     </v-container>
-                            <!--calculate the total-->
-                    <v-container v-for="produit in bill.listeProduits" :key="produit._id">
-                      <p>Total: {{ bill.listeProduits.reduce((total, produit) => total + produit.prixAchat, 0) }}</p>
+                    <hr>
+              
+                    <v-container >
+                      <v-col>
+                      <p  class="total">Total: {{ bill.listeProduits.reduce((total, produit) => total + produit.quantite * produit.prixAchat, 0) }}</p>
+                    </v-col>
                     </v-container>
                 </v-container>
                 </v-row>
+                <v-btn color="primary" text> Print </v-btn>
               </v-container>
             </v-card-text>
           </v-card>
@@ -225,6 +283,7 @@ export default {
     dialog: false,
     dialogEdit: false,
     dialogDelete: false,
+    dialogDetails: false,
     selectedId: null,
     modal: false,
     headers: [
@@ -237,7 +296,6 @@ export default {
       { text: "Added The", value: "date", type: "date", sortable: true },
       { text: "Tracking id", value: "_id" },
       { text: "Number Of Products", value: "listeProduits.length" },
-      { text: "Total Amount", value: "totalAmount" },
       { text: "Actions", value: "actions", sortable: false },
     ],
     total: 0,
@@ -266,7 +324,6 @@ export default {
     this.getBill();
     this.getVendor();
     this.getProduct();
-    // this.mmm();
   },
 
   created() {
@@ -275,27 +332,28 @@ export default {
       console.log(this.getTotal(bill._id));
     });
   },
-  computed:{
-    total(){
-      return this.bills.reduce((total, bill) => {
-        return total + bill.totalAmount;
-      }, 0);
-    }
-  },
+
 
   methods: {
-    // mmm() {
-    //   this.bills.forEach(bill => {
-    //     this.getTotal(bill._id);
-    //     console.log(this.getTotal(bill._id));
-    //   });
-    // },
+    printBill(id) {
+      this.selectedId = id;
+      axios.get(`http://localhost:3000/api/v1/facture/` + id)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error)
+        })
+
+    },
+
     showDetails(id) {
       this.dialogDetails = true;
       this.selectedId = id;
       axios.get(`http://localhost:3000/api/v1/facture/` + id)
         .then((response) => {
           this.bill = response.data.data.facture;
+          console.log(this.bill.listeProduits[0].produit);
         })
         .catch(error => {
           console.log(error)
@@ -324,6 +382,7 @@ export default {
       axios.get('http://localhost:3000/api/v1/produit')
         .then((response) => {
           this.prod = response.data.data.produits;
+          console.log(this.prod);
         })
         .catch(error => {
           console.log(error)
@@ -349,7 +408,6 @@ export default {
     edit(id) {
       axios.patch("http://localhost:3000/api/v1/facture/" + id, this.bill);
       this.closeEdit();
-      this.product.designation = "";
       this.getProduct();
       this.$forceUpdate();
     },
@@ -397,179 +455,16 @@ export default {
 };
 </script>
 
-<script>
-import axios from 'axios';
-export default {
-  data: () => ({
-    dialog: false,
-    dialogEdit: false,
-    dialogDelete: false,
-    selectedId: null,
-    dialogDetails: false,
-    modal: false,
-    headers: [
-      {
-        text: "Vendor",
-        align: "start",
-        sortable: false,
-        value: "fournisseur.name",
-      },
-      { text: "Added The", value: "date", type: "date", sortable: true },
-      { text: "Tracking id", value: "_id" },
-      { text: "Number Of Products", value: "listeProduits.length" },
-      { text: "Total Amount", value: "totalAmount" },
-      { text: "Actions", value: "actions", sortable: false },
-    ],
-    totalAmount: 0,
-    bills: [],
-    vendors: [],
-    prod: [{
-      designation: "",
-      _id: "",
-    }],
-    editedIndex: -1,
-    bill: {
-      id: 0,
-      date: "",
-      fournisseur: "",
-      listeProduits: [
-      ],
 
-    },
-  }),
+<style>
+.total {
+  font-weight: bold;
+  font-size: 1.2em;
+  color: #000;
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 180px;
+}
+  
 
-  watch: {
-    dialog(val) {
-      val || this.close();
-    },
-    dialogDelete(val) {
-      val || this.closeDelete();
-    },
-  },
-
-  mounted() {
-    this.getBill();
-    this.getVendor();
-    this.getProduct();
-    // this.mmm();
-  },
-
-  created() {
-    this.bills.forEach(bill => {
-      this.getTotal(bill._id);
-      console.log(this.getTotal(bill._id));
-    });
-  },
-
-  methods: {
-    // mmm() {
-    //   this.bills.forEach(bill => {
-    //     this.getTotal(bill._id);
-    //     console.log(this.getTotal(bill._id));
-    //   });
-    // },
-    getBill() {
-      axios
-        .get("http://localhost:3000/api/v1/facture")
-        .then((response) => {
-          this.bills = response.data.data.factures;
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
-    getVendor() {
-      axios.get('http://localhost:3000/api/v1/fournisseur')
-        .then((response) => {
-          this.vendors = response.data.data.fournisseurs;
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    },
-    getProduct() {
-      axios.get('http://localhost:3000/api/v1/produit')
-        .then((response) => {
-          this.prod = response.data.data.produits;
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    },
-    getTotal(id) {
-      axios.get('http://localhost:3000/api/v1/facture/factureTotal/' + id)
-        .then((response) => {
-          this.bill.totalAmount = response.data.total;
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    },
-
-    showDetails(id) {
-      this.dialogDetails = true;
-      this.selectedId = id;
-      axios.get('http://localhost:3000/api/v1/facture/' + id)
-        .then((response) => {
-          this.bill = response.data.data.facture;
-          console.log(this.bill)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    },
-    closeDetails() {
-      this.dialogDetails = false;
-    },
-
-    InsertItem(item) {
-      this.dialog = true;
-    },
-    editItem(id) {
-      this.dialogEdit = true;
-      this.selectedId = id;
-    },
-    edit(id) {
-      axios.patch("http://localhost:3000/api/v1/facture/" + id, this.bill);
-      this.closeEdit();
-      this.product.designation = "";
-      this.getProduct();
-      this.$forceUpdate();
-    },
-    deleteItem(id) {
-      this.dialogDelete = true;
-      this.selectedId = id;
-    },
-    deleteItemConfirm(id) {
-      axios.delete("http://localhost:3000/api/v1/facture/" + id);
-      this.closeDelete();
-      this.getBill();
-      this.$forceUpdate();
-    },
-    close() {
-      this.dialog = false;
-    },
-    closeDelete() {
-      this.dialogDelete = false;
-    },
-    closeEdit() {
-      this.dialogEdit = false;
-    },
-    save() {
-      axios
-        .post("http://localhost:3000/api/v1/facture/", this.bill)
-        .then((response) => {
-          console.log(response.data);
-          this.getBill();
-          this.$forceUpdate();
-        })
-        .catch((error) => {
-          console.log(error.response);
-        });
-      this.close();
-      this.getBill();
-      this.$forceUpdate();
-    },
-  },
-};
-</script>
+</style>
